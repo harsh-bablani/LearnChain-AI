e130e50d278fe8da7609a023e7eca4f1334fcc96
HARSH BABLANI <bablaniharsh22@gmail.com>
2025-06-11 17:04:38 +0530

    Update main.tsx

--- learnchain-ai-mentor-main/src/main.tsx
+++ learnchain-ai-mentor-main/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './login.css'; // <- global import
+import './login.css'; // <- globa
 
 
 createRoot(document.getElementById("root")!).render(<App />);
